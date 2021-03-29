b64a3f6c50f3e690aabbd965874a0e16de8c4fce
NA-Siddiky <siddiky.academic@gmail.com>
2021-03-29 23:58:43 +0600

    adding code

---
+++ build/precache-manifest.27334e162b02a65f0b134cefa60b6bdb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fa9b0aa7c9243004382b9351d0391ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91653c13b8eea11ca0c7",
+    "url": "/static/css/main.c06fc45b.chunk.css"
+  },
+  {
+    "revision": "4b93b052d8b03cc779cf",
+    "url": "/static/js/2.8021a195.chunk.js"
+  },
+  {
+    "revision": "e8d667a7629cf3b3c34904b502d8f18b",
+    "url": "/static/js/2.8021a195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91653c13b8eea11ca0c7",
+    "url": "/static/js/main.87e9983e.chunk.js"
+  },
+  {
+    "revision": "e13a02e89408b911631a",
+    "url": "/static/js/runtime-main.3ea24278.js"
+  },
+  {
+    "revision": "e800c8469ca70ca84c718283496ee633",
+    "url": "/static/media/giphy.e800c846.gif"
+  },
+  {
+    "revision": "95f238a51a560680815e09e999af5cb9",
+    "url": "/static/media/logo.95f238a5.png"
+  }
+]);
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCJNAJ13XJFtJO_7jYIPd37S-nJoyPtL2A",
+  authDomain: "ema-john-78e15.firebaseapp.com",
+  projectId: "ema-john-78e15",
+  storageBucket: "ema-john-78e15.appspot.com",
+  messagingSenderId: "1075275769503",
+  appId: "1:1075275769503:web:d0a78151d7339daa9724aa",
+  measurementId: "G-11BMSGPHKF"
+};
+export default firebaseConfig;
